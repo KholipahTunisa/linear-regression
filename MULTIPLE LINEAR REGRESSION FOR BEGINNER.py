a7fbbbcf9a071da0ae3eda5ea3b59e73c1b30d66
kelen <marbunellen@gmail.com>
2020-02-21 15:29:03 +0700

    changing variables after checking assumptions

--- MULTIPLE LINEAR REGRESSION FOR BEGINNER.py
+++ MULTIPLE LINEAR REGRESSION FOR BEGINNER.py
@@ -13,6 +13,7 @@ import statsmodels.api as sm
 import matplotlib.pyplot as plt
 import seaborn as sb
 from numpy.linalg import inv
+import scipy.stats as stats
 
 
 # ##### LOAD RAW DATA TO DATAFRAME
@@ -53,120 +54,20 @@ df.describe(include=['O'])
 df.describe(exclude=['O'])
 
 
-# ##### DATA CLEANING
-
 # In[8]:
 
 
-df.isnull().sum()
+pearsoncorr_raw = df.corr(method='pearson')
+pearsoncorr_raw
 
 
 # In[9]:
 
 
-print (df.shape)
-
-
-# In[10]:
-
-
-#Lets see what percentage of each column has null values
-#It means count number of nulls in every column and divide by total num of rows.
-
-print (df.isnull().sum()/df.shape[0] * 100)
-
-
-# In[11]:
-
-
-cols = [col for col in df.columns if (df[col].isnull().sum()/df.shape[0] * 100 < 100)]
-cols
-
-
-# In[12]:
-
-
-df = df[cols]
-print ('Legitimate columns after dropping null columns: %s' % df.shape[1])
-
-
-# In[13]:
-
-
-df.isnull().sum()
-
-
-# In[14]:
-
-
-df.dtypes
-
-
-# In[15]:
-
-
-#Looks like some columns needs to be converted to numeric field
-df['Date'] = pd.to_datetime(df['Date'])
-df['Precip'] = pd.to_numeric(df['Precip'], errors='coerce')
-df['Snowfall'] = pd.to_numeric(df['Snowfall'], errors='coerce')
-df['PoorWeather'] = pd.to_numeric(df['PoorWeather'], errors='coerce')
-df['PRCP'] = pd.to_numeric(df['PRCP'], errors='coerce')
-df['SNF'] = pd.to_numeric(df['SNF'], errors='coerce')
-df['TSHDSBRSGF'] = pd.to_numeric(df['TSHDSBRSGF'], errors='coerce')
-
-
-# In[16]:
-
-
-#Fill remaining null values. FOr the moment lts perform ffill
-
-df.fillna(method='ffill', inplace=True)
-df.fillna(method='bfill', inplace=True)
-df.isnull().sum()
-
-
-# In[17]:
-
-
-df.dtypes
-
-
-# In[18]:
-
-
-#Plot couple of columns to see how the data is scaled
-
-fig, ax = plt.subplots(4, 2, figsize=(15, 15))
-
-sb.distplot(df['Precip'], ax=ax[0][0])
-#sb.distplot(df['Precip_scaled'], ax=ax[0][1])
-
-sb.distplot(df['MeanTemp'], ax=ax[1][0])
-#sb.distplot(df['MeanTemp_scaled'], ax=ax[1][1])
-
-sb.distplot(df['Snowfall'], ax=ax[2][0])
-#sb.distplot(df['Snowfall_scaled'], ax=ax[2][1])
-
-sb.distplot(df['MAX'], ax=ax[3][0])
-#sb.distplot(df['MAX_scaled'], ax=ax[3][1])
-
-
-# #### CORRELATION
-
-# In[19]:
-
-
-pearsoncorr = df.corr(method='pearson')
-pearsoncorr
-
-
-# In[20]:
-
-
 plt.figure(figsize=(12, 10))
 
-matrix = np.triu(df.corr())
-sb.heatmap(pearsoncorr, 
+matrix_raw = np.triu(df.corr())
+sb.heatmap(pearsoncorr_raw, 
            fmt='.3g', 
            vmin=-1, vmax=1, center= 0,
            annot = False, 
@@ -175,51 +76,361 @@ sb.heatmap(pearsoncorr,
            linewidth = 1)
 
 
+# In[10]:
+
+
+plt.plot(df.DR, df.WindGustSpd, 'o', color='magenta');
+plt.title("Checking Linear Relationship")
+
+
+# In[11]:
+
+
+plt.plot(df.MIN, df.MinTemp, 'o', color='magenta');
+plt.title("Checking Linear Relationship")
+
+
+# In[12]:
+
+
+plt.plot(df.MaxTemp, df.MinTemp, 'o', color='magenta');
+plt.title("Checking Linear Relationship")
+
+### YEAY LINEAR !!!!!!!!!
+
+
+# In[13]:
+
+
+plt.plot(df.MIN, df.MaxTemp, 'o', color='magenta');
+plt.title("Checking Linear Relationship")
+
+### YEAY LINEAR !!!!!!!!!
+
+
+# In[14]:
+
+
+plt.plot(df.MIN, df.WindGustSpd, 'o', color='magenta');
+plt.title("Checking Linear Relationship")
+
+
+# In[15]:
+
+
+df.MIN.unique()
+
+
+# Temporary conclusions:
+# 1. Y: MinTemp, X: MaxTemp, MIN
+
+# ##### DATA CLEANING
+
+# In[16]:
+
+
+df.isnull().sum()
+
+
+# In[17]:
+
+
+print (df.shape)
+
+
+# In[18]:
+
+
+#Lets see what percentage of each column has null values
+#It means count number of nulls in every column and divide by total num of rows.
+
+print (df.isnull().sum()/df.shape[0] * 100)
+
+
+# In[19]:
+
+
+cols = [col for col in df.columns if (df[col].isnull().sum()/df.shape[0] * 100 < 100)]
+cols
+
+
+# In[20]:
+
+
+df_new = df[cols]
+print ('Legitimate columns after dropping null columns: %s' % df_new.shape[1])
+
+
 # In[21]:
 
 
-df.SND.value_counts()
+df_new.isnull().sum()
 
-
-# #### MODEL 1
-# ##### Independent variables: DR, PGT
-# ##### Dependent variables: WindGustSpd
 
 # In[22]:
 
 
-lr = LinearRegression()
-x = df[['DR','PGT']] # here we have 2 variables for multiple regression
-                        # if you just want to use one variable for simple linear regression
-x
+df_new.dtypes
 
 
 # In[23]:
 
 
-x.shape
+#Looks like some columns needs to be converted to numeric field
+df_new['Date'] = pd.to_datetime(df_new['Date'])
+df_new['Precip'] = pd.to_numeric(df_new['Precip'], errors='coerce')
+df_new['Snowfall'] = pd.to_numeric(df_new['Snowfall'], errors='coerce')
+df_new['PoorWeather'] = pd.to_numeric(df_new['PoorWeather'], errors='coerce')
+df_new['PRCP'] = pd.to_numeric(df_new['PRCP'], errors='coerce')
+df_new['SNF'] = pd.to_numeric(df_new['SNF'], errors='coerce')
+df_new['TSHDSBRSGF'] = pd.to_numeric(df_new['TSHDSBRSGF'], errors='coerce')
 
 
 # In[24]:
 
 
-y = df.WindGustSpd.values.reshape(-1,1)
-y
+#Fill remaining null values. FOr the moment lts perform ffill
+
+df_new.fillna(method='ffill', inplace=True)
+df_new.fillna(method='bfill', inplace=True)
+df_new.isnull().sum()
 
 
 # In[25]:
 
 
-y.shape
+df_new.dtypes
 
 
 # In[26]:
 
 
+#Plot couple of columns to see how the data is scaled
+
+fig, ax = plt.subplots(4, 2, figsize=(15, 15))
+
+sb.distplot(df_new['Precip'], ax=ax[0][0])
+#sb.distplot(df['Precip_scaled'], ax=ax[0][1])
+
+sb.distplot(df_new['MeanTemp'], ax=ax[1][0])
+#sb.distplot(df['MeanTemp_scaled'], ax=ax[1][1])
+
+sb.distplot(df_new['Snowfall'], ax=ax[2][0])
+#sb.distplot(df['Snowfall_scaled'], ax=ax[2][1])
+
+sb.distplot(df_new['MAX'], ax=ax[3][0])
+#sb.distplot(df['MAX_scaled'], ax=ax[3][1])
+
+
+# #### CORRELATION
+
+# In[27]:
+
+
+pearsoncorr_new = df_new.corr(method='pearson')
+pearsoncorr_new
+
+
+# In[28]:
+
+
+plt.figure(figsize=(12, 10))
+
+matrix_new = np.triu(df_new.corr())
+sb.heatmap(pearsoncorr_new, 
+           fmt='.3g', 
+           vmin=-1, vmax=1, center= 0,
+           annot = False, 
+           square = True, 
+           cmap = 'coolwarm',
+           linewidth = 1)
+
+
+# In[29]:
+
+
+df_new.SND.value_counts()
+
+
+# #### MODEL 1
+# ##### Independent variables: MIN, MaxTemp
+# ##### Dependent variables: MinTemp
+
+# ##### CHECKING LINEAR REGRESSION ASSUMPTIONS
+
+# 1. Investigating a Linear Relationship
+
+# In[30]:
+
+
+plt.plot(df_new.MIN, df_new.MinTemp, '.', color='magenta');
+plt.title("Checking Linear Relationship MIN and MinTemp")
+
+
+# In[31]:
+
+
+plt.plot(df_new.MaxTemp, df_new.MinTemp, '.', color='green');
+plt.title("Checking Linear Relationship MaxTemp and MinTemp")
+
+
+# 2. Variables should follow a Normal Distribution ############# WRONG! YOU JUST HAVE TO CHECK NORMALITY AFTER REGRESSION BECAUSE THE THING THAT HAS TO BE NORMAL IS THE ERROR! OMG HELLOOOO :)
+
+# In[ ]:
+
+
+stats.probplot(df_new.MinTemp, dist="norm", plot=plt)
+plt.show()
+
+
+# In[ ]:
+
+
+stats.probplot(df_new.MIN, dist="norm", plot=plt)
+plt.show()
+
+
+# In[ ]:
+
+
+stats.probplot(df_new.MaxTemp, dist="norm", plot=plt)
+plt.show()
+
+
+# In[ ]:
+
+
+# Anderson-Darling Test
+
+from scipy.stats import anderson
+
+# normality test
+result = anderson(df_new.MinTemp)
+print('Statistic: %.3f' % result.statistic)
+p = 0
+for i in range(len(result.critical_values)):
+    sl, cv = result.significance_level[i], result.critical_values[i]
+    if result.statistic < result.critical_values[i]:
+        print('%.3f: %.3f, data looks normal (fail to reject H0)' % (sl, cv))
+    else:
+        print('%.3f: %.3f, data does not look normal (reject H0)' % (sl, cv))
+
+
+# In[ ]:
+
+
+# Anderson-Darling Test
+
+from scipy.stats import anderson
+
+# normality test
+result = anderson(df_new.MaxTemp)
+print('Statistic: %.3f' % result.statistic)
+p = 0
+for i in range(len(result.critical_values)):
+    sl, cv = result.significance_level[i], result.critical_values[i]
+    if result.statistic < result.critical_values[i]:
+        print('%.3f: %.3f, data looks normal (fail to reject H0)' % (sl, cv))
+    else:
+        print('%.3f: %.3f, data does not look normal (reject H0)' % (sl, cv))
+
+
+# In[ ]:
+
+
+# Anderson-Darling Test
+
+# normality test
+result = anderson(df_new.MIN)
+print('Statistic: %.3f' % result.statistic)
+p = 0
+for i in range(len(result.critical_values)):
+    sl, cv = result.significance_level[i], result.critical_values[i]
+    if result.statistic < result.critical_values[i]:
+        print('%.3f: %.3f, data looks normal (fail to reject H0)' % (sl, cv))
+    else:
+        print('%.3f: %.3f, data does not look normal (reject H0)' % (sl, cv))
+
+
+# In[ ]:
+
+
+# Shapiro-Wilk Test:  the test may be suitable for smaller samples of data, e.g. thousands of observations or fewer.
+
+# normality test
+stat, p = shapiro(df_new.MinTemp)
+print('Statistics=%.3f, p=%.3f' % (stat, p))
+
+# interpret
+alpha = 0.05
+if p > alpha:
+    print('Sample looks Gaussian (fail to reject H0)')
+else:
+    print('Sample does not look Gaussian (reject H0)')
+
+
+# In[ ]:
+
+
+# D'Agostino and Pearson's Test
+
+from scipy.stats import normaltest
+
+# normality test
+stat, p = normaltest(df_new.MinTemp)
+print('Statistics=%.3f, p=%.3f' % (stat, p))
+
+# interpret
+alpha = 0.05
+if p > alpha:
+    print('Sample looks Gaussian (fail to reject H0)')
+else:
+    print('Sample does not look Gaussian (reject H0)')
+
+
+# ***WHAT AM I DOING THO HAHAHA*** SKIP
+
+# 2. Multicollinearity Test #### DONE BEFORE: CORRELATION
+
+# 3. Multicollinearity Test #### DONE BEFORE: CORRELATION
+
+# ##### USING SCIKIT LEARN PACKAGE
+
+# In[32]:
+
+
+lr = LinearRegression()
+x = df_new[['MIN','MaxTemp']] # here we have 2 variables for multiple regression
+                        # if you just want to use one variable for simple linear regression
+x
+
+
+# In[33]:
+
+
+x.shape
+
+
+# In[34]:
+
+
+y = df.MinTemp.values.reshape(-1,1)
+y
+
+
+# In[35]:
+
+
+y.shape
+
+
+# In[36]:
+
+
 lr.fit(x,y)
 
 
-# In[27]:
+# In[37]:
 
 
 from sklearn.model_selection import train_test_split
@@ -232,34 +443,34 @@ a = lr.predict(x_test)
 a
 
 
-# In[28]:
+# In[38]:
 
 
 mean_absolute_error(a,y_test)
 
 
-# In[29]:
+# In[39]:
 
 
 x = sm.add_constant(x) # adding a constant
 x
 
 
-# In[30]:
+# In[40]:
 
 
 model = sm.OLS(y, x).fit()
 model
 
 
-# In[31]:
+# In[41]:
 
 
 predictions = model.predict(x) 
 predictions
 
 
-# In[32]:
+# In[42]:
 
 
 print(model.summary())
@@ -267,14 +478,14 @@ print(model.summary())
 
 # ##### MANUALLY
 
-# In[33]:
+# In[55]:
 
 
-data1 = df[['DR','PGT','WindGustSpd']]
+data1 = df_new[['MIN','MaxTemp','MinTemp']]
 data1
 
 
-# In[34]:
+# In[56]:
 
 
 #convert dataframe to numpy matrix
@@ -282,99 +493,55 @@ dataM = data1.as_matrix()
 dataM
 
 
-# In[35]:
+# In[57]:
 
 
 X_manual, y_manual = dataM[:,[1,2]], dataM[:,0]
 
 
-# In[37]:
+# In[58]:
+
+
+X_manual
+
+
+# In[59]:
+
+
+y_manual
+
+
+# In[60]:
 
 
 x0 = np.ones((len(X_manual), 1), dtype=int)
 x0
 
 
-# In[38]:
+# In[61]:
 
 
 x_manual = np.concatenate((x0, X_manual), axis=1)
 x_manual
 
 
-# In[40]:
+# In[62]:
 
 
 b = inv(x_manual.T.dot(x_manual)).dot(x_manual.T).dot(y_manual)
 b
 
 
-# In[41]:
+# In[63]:
 
 
 yhat = x_manual.dot(b)
 yhat
 
 
-# In[44]:
+# In[64]:
 
 
 error = y_manual-yhat
 error
-
-
-# #### MODEL 2
-# ##### Independent variables: DR, TSHDSBRSGF
-# ##### Dependent variables: WindGustSpd
-
-# In[45]:
-
-
-x2 = df[['DR','TSHDSBRSGF']]
-x2
-
-
-# In[46]:
-
-
-lr.fit(x2,y)
-
-x2_train,x2_test,y2_train,y2_test = train_test_split(x2,y,test_size=0.0005)
-
-lr.fit(x2_train,y2_train)
-a2 = lr.predict(x2_test)
-a2
-
-
-# In[47]:
-
-
-mean_absolute_error(a2,y2_test)
-
-
-# In[48]:
-
-
-x2 = sm.add_constant(x2) # adding a constant
-x2
-
-
-# In[49]:
-
-
-model2 = sm.OLS(y, x2).fit()
-model2
-
-
-# In[50]:
-
-
-predictions2 = model.predict(x2) 
-predictions2
-
-
-# In[51]:
-
-
-print(model2.summary())
 
